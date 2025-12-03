8ef19e1807a9f0f63a29874d252afd180d281aec
ultramanvssupersentai-ux <ultramanvssupersentai@gmail.com>
2025-12-03 17:33:10 +0700

    Initialize Flutter app with Firebase and Riverpod

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'screens/lobby_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'War Card Game',
+      theme: ThemeData.dark(),
+      home: LobbyScreen(),
+    );
+  }
+}
